e5bf663e61ac137400168c23697069e17836f070
Nicola Cuomo <nicola.cuomo@iwd.io>
2024-08-06 15:37:04 +0200

    using vercel/postgres instead og sqlite

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,6 @@
 import * as schema from "./schema";
 
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-import { env } from "@/env";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
 
-export const client = createClient({
-  url: env.DATABASE_URL!,
-  authToken: env.DB_AUTH_TOKEN!,
-});
-
-export const db = drizzle(client, { schema });
+export const db = drizzle(sql, { schema });
